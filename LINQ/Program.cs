69605b4c30e36515efd88a3462bfaccebe5a1267
Austin Okyere <okyereaustin@gmail.com>
2026-01-13 20:07:52 -0700

    Completed Linq exercise

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var sports = new List<string>() {"Basketball", "Football", "Golf", "Baseball", "Hockey", "Track"};
+
+            var longerNames = sports.OrderByDescending(x => x.Length).ToList();
+            
+            longerNames.ForEach(x => Console.WriteLine(x));
         }
     }
 }
